932411b438137df489f420e30be927e8dc9e08dd
tangwenchuan <wenchuan.tang@xinfei.cn>
2025-05-28 18:54:14 +0800

    go

--- study/l6_1_Func/Demo1.go
+++ study/l6_1_Func/Demo1.go
@@ -1,11 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 
-	F3 := func(a int, b int) {
-
-	}
-	F3(1, 2)
+	var f F3 = myF3 // 需要和F3具有相同的形参，才能被赋值给F3类型的变量
+	f(1, 2)
 }
 
 // go 不支持函数的重载，即不同的函数，方法名一定不一样
@@ -15,5 +15,11 @@ func f1() {}
 
 func f2() {}
 
-// 只声明一个函数，而没有函数体
+// 只声明一个函数，而没有函数体，F3就是一种函数类型。类似于Java中的接口定义
 type F3 func(a int, b int) (int, int)
+
+// 需要和F3具有相同的形参，才能被赋值给F3类型的变量
+func myF3(a int, b int) (int, int) {
+	fmt.Println("this is myF3")
+	return a, b
+}
